b674bee91b4b5d3346137c61f60e013c86dd85a8
ramjeerk12 <ramjeekumarrk7261@gmail.com>
2026-04-14 00:14:17 +0530

    Add A. Blocked.cpp

---
+++ codeforces/2026/round_1092/A. Blocked.cpp
@@ -0,0 +1,40 @@
+// Contest: Codeforces Round 1092
+// File: A. Blocked.cpp 
+// Date: 2026-04-13
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fast ios::sync_with_stdio(false); cin.tie(NULL);
+
+void solve(){
+    int n;
+    cin >> n;
+
+    vector<int> a(n, 0);
+    for(int i = 0; i < n; i++) cin >> a[i];
+
+    sort(a.begin(), a.end(), greater<int>());
+    bool isImpossible = false;
+
+    for(int i = 0; i < n-1; i++){
+        if(a[i] == a[i+1]){
+            isImpossible = true;
+            break;
+        }
+    }
+
+    if(isImpossible) cout << "-1\n";
+    else {
+        for(auto val : a) cout << val << " ";
+        cout << "\n";
+    }
+}
+
+int main() {
+    fast;
+    int t; cin >> t;
+    while(t--) {
+        solve();
+    }
+}
